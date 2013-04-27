c11626dc37965494f265ec8c9b03c26174a7880c
Paul Varcholik <pvarchol@bespokesoftware.org>
2013-04-27 13:34:48 -0400

    CS Demo

---
+++ Samples/CS/Transmitter/UdpTransmitter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+using Bespoke.Common;
+using Bespoke.Common.Osc;
+
+namespace Transmitter
+{
+    public class UdpTransmitter : ITransmitter
+    {
+        public void Start(OscPacket packet)
+        {
+            Assert.ParamIsNotNull(packet);
+
+            mPacket = packet;
+            OscPacket.UdpClient= new UdpClient(SourcePort);
+            mSendMessages = true;
+
+            mTransmitterThread = new Thread(RunWorker);
+            mTransmitterThread.Start();
+        }
+
+        public void Stop()
+        {
+            mSendMessages = false;
+            mTransmitterThread.Join();
+        }
+
+        private void RunWorker()
+        {
+            try
+            {
+                while (mSendMessages)
+                {
+                     mPacket.Send(Destination);
+
+                    mTransmissionCount++;
+                    Console.Clear();
+                    Console.WriteLine("Osc Transmitter: Udp");
+                    Console.WriteLine("Transmission Count: {0}\n", mTransmissionCount);
+                    Console.WriteLine("Press any key to exit.");
+
+                    Thread.Sleep(1000);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        private static readonly IPEndPoint Destination = new IPEndPoint(IPAddress.Loopback, Program.Port);
+        private static readonly int SourcePort = 10024;
+
+        private volatile bool mSendMessages;
+        private Thread mTransmitterThread;
+        private OscPacket mPacket;
+        private int mTransmissionCount;
+    }
+}
